93bd4e2190ea5d7655f2ac9c557f71b1f57ed0cd
hg00521@naver.com <hg00521@naver.com>
2019-08-28 14:14:21 +0900

    ..

--- ShopInShop-3/src/main/java/org/sis/config/MapperConfig.java
+++ ShopInShop-3/src/main/java/org/sis/config/MapperConfig.java
@@ -26,6 +26,7 @@ public class MapperConfig {
 	        return sessionFactory.getObject();
 	    }
 	 
+	 	
 	 @Bean
 	    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) throws Exception {
 	      final SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory);
--- ShopInShop-3/src/main/java/org/sis/user/controller/UserController.java
+++ ShopInShop-3/src/main/java/org/sis/user/controller/UserController.java
@@ -1,40 +1,29 @@
 package org.sis.user.controller;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.sis.mapper.ReplyMapper;
 import org.sis.user.model.MemberVO;
 import org.sis.user.oauth.KakaoApi;
 import org.sis.user.oauth.NaverLoginBO;
 import org.sis.user.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.scribejava.core.model.OAuth2AccessToken;
 
-import lombok.AllArgsConstructor;
 import lombok.Setter;
 import lombok.extern.java.Log;
 
@@ -43,7 +32,7 @@ import lombok.extern.java.Log;
 @Controller
 @RequestMapping("user/*")
 @Log
-public class LoginController {
+public class UserController {
 
 	@Setter(onMethod_=@Autowired )
 	private LoginService service;
@@ -156,6 +145,15 @@ public class LoginController {
 		
 		
 	}
+	
+	@GetMapping("/mypage")
+	public void mypage() {
+
+		log.info("mypage...");
+		
+		
+	}
+	
 
 	@RequestMapping(value = "/oauth")
 	public String login(@RequestParam("code") String code, HttpSession session) {
