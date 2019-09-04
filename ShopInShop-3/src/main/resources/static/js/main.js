030eefd314ea72d0e41a1ce9da78a584a15bca8e
Leejunhyuck <hg00521@naver.com>
2019-09-04 16:28:21 +0900

    store serachmenu bar

--- ShopInShop-3/src/main/resources/static/js/main.js
+++ ShopInShop-3/src/main/resources/static/js/main.js
@@ -160,6 +160,31 @@ jQuery(document).ready(function($) {
     });
     $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
       " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    
+    $( "#slider-range2" ).slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [ 75, 300 ],
+        slide: function( event, ui ) {
+          $( "#amount2" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        }
+      });
+      $( "#amount2" ).val( "$" + $( "#slider-range2" ).slider( "values", 0 ) +
+        " - $" + $( "#slider-range2" ).slider( "values", 1 ) );
+    
+      
+      $( "#slider-range3" ).slider({
+          range: true,
+          min: 0,
+          max: 500,
+          values: [ 75, 300 ],
+          slide: function( event, ui ) {
+            $( "#amount3" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+          }
+        });
+        $( "#amount3" ).val( "$" + $( "#slider-range3" ).slider( "values", 0 ) +
+          " - $" + $( "#slider-range3" ).slider( "values", 1 ) );
 	};
 	siteSliderRange();
 
